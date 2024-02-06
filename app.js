2bf703a3033f0780ec26fb98e76d6707512af60e
killaterminal <157806879+killaterminal@users.noreply.github.com>
2024-02-06 17:35:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -142,6 +142,21 @@ async function comoFuncionaElPrograma(chatId, callbackQuery) {
     bot.answerCallbackQuery(callbackQuery.id);
 }
 
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === 'Escríbeme ✍️') {
+        User.updateOne({ chatId: chatId.toString() }, { directed: true })
+            .then(() => {
+                console.log('Directed updated successfully');
+            })
+            .catch((error) => {
+                console.error('Error updating directed:', error);
+            });
+    }
+});
+
 bot.on('callback_query', (callbackQuery) => {
     const chatId = callbackQuery.message.chat.id;
     const action = callbackQuery.data;
